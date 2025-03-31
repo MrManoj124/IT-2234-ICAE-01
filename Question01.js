c9407b19121b3e15aff25b957763395c5e4d6e28
Manorooban <manojmanorooban@gmail.com>
2025-03-31 18:36:22 +0000

    create  a function called getmaxPurchase

--- Question01.js
+++ Question01.js
@@ -1,1 +1,15 @@
 function getMaxPurchase(budget, keyboardPrices, mousePrices)
+{
+    let maxPurchase = -1;
+
+    for (let k of keyboardPrices) {
+        for (let m of mousePrices) {
+            let totalCost = k + m;
+            if (totalCost <= budget && totalCost > maxPurchase) {
+                maxPurchase = totalCost;
+            }
+        }
+    }
+
+    return maxPurchase;
+}
