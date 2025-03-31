98bea74e2ca2c76253d25548398f6d88c6139977
Manorooban <manojmanorooban@gmail.com>
2025-03-31 18:36:48 +0000

    print the function

--- Question01.js
+++ Question01.js
@@ -13,3 +13,7 @@ function getMaxPurchase(budget, keyboardPrices, mousePrices)
 
     return maxPurchase;
 }
+
+console.log(getMaxPurchase(60, [40, 50, 60], [5, 8, 12]));
+console.log(getMaxPurchase(10, [3, 1], [5, 2, 8])); 
+console.log(getMaxPurchase(20, [30, 15], [8, 10, 6])); 
